15adb0464d2de1e71bffcb4d9cd0ba72fd54847c
zachrab <zachrab@gmail.com>
2013-04-03 19:42:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullStackChallenge::Application.config.secret_token = 'ea22bc2ac6cae54bf7b0cd6d54fd5825e81af7109578163eb55668d3d1f283bcf10b43ea8e7f4f4d098c1cf0131aa1302b1e976f0accb07a659cae2aeba94d23'
