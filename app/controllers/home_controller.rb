ca341d22bbbd0d988c2691279fc41320cd91e810
zachrab <zachrab@gmail.com>
2013-04-05 01:39:51 -0700

    added tweet stream and search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @search = "America"
+  end
+  
+  def search
+    @search = params[:search].blank? ? "America" : params[:search]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ FullStackChallenge::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   root :to => 'home#index'
+  match 'search' => 'home#search'
 
   # See how all your routes lay out with "rake routes"
 
